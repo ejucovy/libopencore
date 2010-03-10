e17a9dfa3e9db525d56681594b9eb4195148c214
egj <egj@40f73b84-6177-4a8e-8c61-7eee4b1974ed>
2010-03-10 16:12:11 +0000

    guarantee that the rewritten url to opencore always includes port information for the VHM, even if it's not in HTTP_HOST
    git-svn-id: https://svn.openplans.org/svn/opencore/libopencore/trunk@26475 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- libopencore/http_proxy.py
+++ libopencore/http_proxy.py
@@ -1,6 +1,6 @@
 from wsgifilter import proxyapp
 
-vhm_template = "VirtualHostBase/%(wsgi.url_scheme)s/%(HTTP_HOST)s/openplans/VirtualHostRoot/"
+vhm_template = "VirtualHostBase/%(wsgi.url_scheme)s/%(HTTP_HOST)s:%(frontend_port)s/openplans/VirtualHostRoot/"
 
 def app_factory(global_conf,
                 remote_uri=None, is_opencore=None,
@@ -15,7 +15,23 @@ class RemoteProxy(object):
     def __call__(self, environ, start_response):
         remote_uri = self.remote_uri
         if self.is_opencore:
-            remote_uri = remote_uri + (vhm_template % environ)
+            environ_copy = environ.copy()
+
+            # With varnish on port 80 proxying to the opencore stack entrypoint,
+            # HTTP_HOST doesn't include the :80 bit. (I don't know about other
+            # frontends.) Just to be safe, we'll decompose HTTP_HOST into its
+            # parts, and if the port information is missing, we'll set port 80.
+            #
+            # The virtual host monster needs this information. If it's missing,
+            # opencore will generate links with the port that Zope is served on.
+
+            parts = environ['HTTP_HOST'].split(':')
+            environ_copy['HTTP_HOST'] = parts[0]
+            if len(parts) > 1:
+                environ_copy['frontend_port'] = parts[1]
+            else:
+                environ_copy['frontend_port'] = '80'
+            remote_uri = remote_uri + (vhm_template % environ_copy)
 
         app = proxyapp.ForcedProxy(
             remote=remote_uri,
